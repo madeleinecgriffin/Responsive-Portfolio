d426835a6703aab25cd3fd724599825279e32fd0
Madeleine Griffin <madeleinecgriffin@gmail.com>
2018-05-27 15:10:35 -0500

    fin

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Responsive-Portfolio
-responsive portfolio - no bootstrap
+# Responsive Portfolio
+A clean, streamlined portfolio of my best work on Github and Heroku to show to others.
+
+### Getting Started
+This portfolio can be viewed live on Heroku. If you would like to run this locally, simply download, go into the portfolio folder, run "yarn" in your terminal to install packages, and then run "yarn start" to deploy to your local host. This portfolio is front end only.
--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -52,17 +52,24 @@ class App extends Component {
     return (
       <Grid componentClass="Container">
         <Row>
-          <Col md={1} xsHidden sHidden style={{ height: '100px' }}></Col>
-          <Col md={10} sm={12}>
-            <Row style={{ borderBottom: 'solid 1px black', marginBottom: '20px' }}>
-              <Col md={4} sm={12}>
-                <Title>Madeleine Griffin</Title>
-              </Col>
-              <Col md={3} sm={12}>
-                <About>a full stack web developer based in Chicago</About>
+          <Col md={1} sm={1} style={{ height: '10px' }}></Col>
+          <Col md={10} sm={10}>
+            <Row style={{ borderBottom: 'solid 1px black', }}>
+              <Col md={7} sm={12}>
+                <Row>
+                  <Col md={12} sm={12}>
+                    <Title>Madeleine Griffin</Title>
+                  </Col>
+                </Row>
+                <Row style={{ paddingBottom: '5px' }}>
+                  <Col md={12} sm={12}>
+                    <About><h5>a full stack web developer based in Chicago</h5></About>
+                  </Col>
+                </Row>
               </Col>
               <Col md={5} sm={12}>
                 <Contact>
+                  <Row style={{ height: '15px' }}></Row>
                   <Row>
                     <Col md={6} sm={12}>
                       <ListLink href={'https://www.linkedin.com/in/madeleinegriffin'}>LINKEDIN</ListLink>
@@ -75,43 +82,65 @@ class App extends Component {
               </Col>
             </Row>
           </Col>
-          <Col md={1} xsHidden sHidden style={{ height: '100px' }}></Col>
+          <Col md={1} sm={1} style={{ height: '10px' }}></Col>
         </Row>
 
         <Row>
-          <Col md={2} xsHidden sHidden style={{ height: '100px' }}></Col>
-          <Col md={8} sm={12}>
-            <Row style={{ borderBottom: 'solid 1px black', marginBottom: '20px', paddingBottom: '10px' }}>
-              <Col md={6} sm={12}>
-                <ToggleApp onClick={() => this.changeApp("app")}>FULLSTACK APPS</ToggleApp>
-              </Col>
-              <Col md={6} sm={12}>
-                <ToggleApp onClick={() => this.changeApp("game")}>GAMES / EXERCISES</ToggleApp>
+          <Col md={2} sm={1} style={{ height: '10px' }}></Col>
+          <Col md={8} sm={10}>
+            <Row style={{ borderBottom: 'solid 1px black', marginBottom: '20px', marginTop: '10px', paddingBottom: '10px', paddingTop: '10px' }}>
+              <Col md={12} sm={12}>
+
+                <Row>
+                <Col md={3} sm={1} style={{ height: '10px' }}></Col>
+                  <Col md={6} sm={10}>
+                    <About>
+                      <h4><p style={{ textAlign: 'center', paddingTop: '20px'}}>Which projects would you like to view?</p></h4>
+                    </About>
+                  </Col>
+                  <Col md={3} sm={1} style={{ height: '10px' }}></Col>
+
+                </Row>
+                <Row>
+                  <Col md={6} sm={12}>
+                    <ToggleApp onClick={() => this.changeApp("app")}>FULLSTACK APPS</ToggleApp>
+                  </Col>
+                  <Col md={6} sm={12}>
+                    <ToggleApp onClick={() => this.changeApp("game")}>GAMES / EXERCISES</ToggleApp>
+                  </Col>
+                </Row>
               </Col>
             </Row >
+            <Col md={2} sm={1} style={{ height: '10px' }}></Col>
+
             <Row>
-              <List>
-                {this.state.displayProjects.map(project => (
-                  <Row>
-                    <ListItem
-                      key={project.id}
-                      id={project.id}
-                    >
-                      <Row>
-                        <Col md={8} sm={12}>
-                          <ListTitle><h2>{project.title}</h2></ListTitle>
-                          <ListTitle>Tools: {project.tools}</ListTitle>
-                          <ListTitle>Completed: {project.date}</ListTitle>
-                        </Col>
-                        <Col md={4} sm={12}>
-                          <ListLink href={project.github}>GITHUB</ListLink>
-                          <ListLink href={project.live}>LIVE</ListLink>
-                        </Col>
-                      </Row>
-                    </ListItem>
-                  </Row>
-                ))}
-              </List>
+              <Col md={1} sm={1} style={{ height: '10px' }}></Col>
+              <Col md={10} sm={10}>
+                <List>
+                  {this.state.displayProjects.map(project => (
+                    <Row>
+                      <ListItem
+                        key={project.id}
+                        id={project.id}
+                      >
+                        <Row>
+                          <Col md={8} sm={12}>
+                            <ListTitle><h2>{project.title}</h2></ListTitle>
+                            <ListTitle>Tools: {project.tools}</ListTitle>
+                            <ListTitle>Completed: {project.date}</ListTitle>
+                          </Col>
+                          <Col md={4} sm={12}>
+                            <ListLink href={project.github}>GITHUB</ListLink>
+                            <ListLink href={project.live}>LIVE</ListLink>
+                          </Col>
+                        </Row>
+                      </ListItem>
+                    </Row>
+                  ))}
+                </List>
+              </Col>
+              <Col md={1} sm={1} style={{ height: '10px' }}></Col>
+
             </Row>
           </Col>
           <Col md={2} xsHidden sHidden style={{ height: '100px' }}></Col>
